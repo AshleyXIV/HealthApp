94a9fb00e4709ed7f8a93e9829793daaa2619663
jentatl <jentatl@users.noreply.github.com>
2019-04-27 19:16:00 -0400

    basic user profile set up

--- app/src/main/java/cs/uga/edu/healthapp/ProfileFragment.java
+++ app/src/main/java/cs/uga/edu/healthapp/ProfileFragment.java
@@ -1,17 +1,72 @@
 package cs.uga.edu.healthapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView profileEmail, profileName, profileHeight, profileWeight;
+    private Button profileUpdate;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase firebaseDatabase;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profileEmail = view.findViewById(R.id.textViewEmail);
+        profileName = view.findViewById(R.id.textViewName);
+        profileHeight = view.findViewById(R.id.textViewHeight);
+        profileWeight = view.findViewById(R.id.textViewWeight);
+        profileUpdate = view.findViewById(R.id.buttonProfileUpdate);
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        //reference the database with the specific user's unique id
+        DatabaseReference databaseReference = firebaseDatabase.getReference(mAuth.getUid());
+        //event listener for referencing the database
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {  //whenever there is a change in the database or when the app just starts
+                User user = dataSnapshot.getValue(User.class);  //retrieve all of the user's info from the database and place in user object
+
+                    profileEmail.setText("Email: " + user.getEmail());
+                    profileName.setText("Name: " + user.getName());
+                    profileHeight.setText("Height: " + user.getHeight());
+                    profileWeight.setText("Weight: " + user.getWeight());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { //if there is some database error, show toast error
+                Toast.makeText(getActivity(), databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
     }
 }
--- app/src/main/java/cs/uga/edu/healthapp/User.java
+++ app/src/main/java/cs/uga/edu/healthapp/User.java
@@ -4,7 +4,7 @@ public class User {
     public String email, name, height, weight;
 
     public User(){
-
+        //required default constructor
     }
 
     public User(String email, String name, String height, String weight){
@@ -14,4 +14,35 @@ public class User {
         this.weight = weight;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
 }
