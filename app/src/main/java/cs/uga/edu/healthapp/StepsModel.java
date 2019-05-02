4b1c5f4d55c7b476d2cfda66b8c7dd8d57cdd06a
jentatl <jentatl@users.noreply.github.com>
2019-05-01 21:48:08 -0400

    stepsWalked data now saves to firebase

--- app/src/main/java/cs/uga/edu/healthapp/StepsModel.java
+++ app/src/main/java/cs/uga/edu/healthapp/StepsModel.java
@@ -2,5 +2,14 @@ package cs.uga.edu.healthapp;
 
 public class StepsModel {
 
-    public String date, stepsWalked;
+    public String dateLabel, date, stepsLabel, stepsWalked;
+
+    StepsModel(){
+
+    }
+
+    StepsModel(String date, String stepsWalked){
+        this.date = date;
+        this.stepsWalked = stepsWalked;
+    }
 }
